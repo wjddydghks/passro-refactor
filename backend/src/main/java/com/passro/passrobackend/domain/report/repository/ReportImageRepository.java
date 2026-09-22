98006c169d8ddd65f8bc4f7de70e9a3bc0b09663
wjddydghks <wjddydghks0801@gmail.com>
2026-09-22 22:05:41 +0900

    버그 수정

--- backend/src/main/java/com/passro/passrobackend/domain/report/repository/ReportImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.passro.passrobackend.domain.report.repository;
-
-import com.passro.passrobackend.domain.report.entity.ReportImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportImageRepository extends JpaRepository<ReportImage, Long> {
-}
--- src/main/java/com/passro/passrobackend/global/code/BaseSuccessCode.java
+++
@@ -1,9 +0,0 @@
-package com.passro.passrobackend.global.code;
-
-import org.springframework.http.HttpStatus;
-
-public interface BaseSuccessCode {
-    HttpStatus getStatus();
-    String getCode();
-    String getMessage();
-}
